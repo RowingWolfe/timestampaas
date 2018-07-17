c8900c8ba88898011d2d4ea32cea45ae8647f651
Roanoke <hoodwolf@roanoke.space>
2018-07-17 15:54:13 -0400

    Basic functionality complete

---
+++ index.js
@@ -0,0 +1,28 @@
+//Skulljack labs Timestamp service
+
+const express = require('express');
+const moment = require('moment');
+const app = express();
+
+let time = moment();
+const dateFormat = "dddd, M/D/YYYY H:mm:ss a";
+
+app.get('/api/timestamp', (req, res, next) => {
+    res.send({'unix': time.valueOf(), 'utc': time.format(dateFormat)});
+});
+
+app.get('/api/timestamp/:timestring/', (req, res, next)=> {
+    if(req.params.timestring){
+        time = moment(req.params.timestring).valueOf();
+    }
+
+    let parsedTime = moment(time).format(dateFormat);
+
+    if( parsedTime === 'Invalid date'){
+        res.send({'error': 'Invalid date'})
+    }else{
+        res.send({'unix':time, 'utc': parsedTime});
+    }
+});
+
+app.listen(3000);
